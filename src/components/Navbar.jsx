3f10378584733d837dfcf3648357cd393938a4fa
Charuka Wijethunga <charukamwijethunga@gmail.com>
2025-11-07 00:27:11 +0530

    Update logo X

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'; // <-- Import the CSS file
 const Navbar = ({ content }) => {
   return (
     <nav>
-      <a href="#home" className="logo">{content.logo}</a>
+      <a href="#home" ><span className="logo">CeyCode</span><span className='last-letter'>X</span></a>
       <ul>
         {content.links.map((link) => (
           <li key={link.text}>
